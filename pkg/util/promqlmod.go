a4ff3df7e009bd6a90c792646864b1e55f30ed6b
Joachim Barheine <joachim.barheine@sap.com>
2017-06-09 10:26:09 +0200

    migrate to glide

--- pkg/util/promqlmod.go
+++ pkg/util/promqlmod.go
@@ -6,55 +6,19 @@ import (
 	"github.com/prometheus/prometheus/storage/metric"
 )
 
-// AddLabelConstraintToExpression enhances a PromQL expression to limit it to series matching a certain label
-func AddLabelConstraintToExpression(expression string, key string, value string) (string, error) {
-	exprNode, err := promql.ParseExpr(expression)
-	if err != nil {
-		return "", err
-	}
-	matcher, err := metric.NewLabelMatcher(metric.Equal, model.LabelName(key), model.LabelValue(value))
-	if err != nil {
-		return "", err
-	}
-
-	// since to structure of the expression is not modified we can use a visitor, avoiding our own traversal code
-	promql.Walk(labelInjector{matcher: matcher}, exprNode)
-
-	return exprNode.String(), nil
+func AddLabelConstraint(node promql.Node, key string, value string) {
+	// matcher := model.Matcher{model.LabelName(key), value, false}
+	matcher, _ := metric.NewLabelMatcher(metric.Equal, model.LabelName(key), model.LabelValue(value))
+	promql.Walk(labelInjector{matcher: matcher}, node)
 }
 
-// AddLabelConstraintToSelector enhances a PromQL selector with an additional label selector
-func AddLabelConstraintToSelector(metricSelector string, key string, value string) (string, error) {
-	matcher, err := metric.NewLabelMatcher(metric.Equal, model.LabelName(key), model.LabelValue(value))
-	if err != nil {
-		return "", err
-	}
-
-	var labelMatchers metric.LabelMatchers
-	if metricSelector != "{}" {
-		labelMatchers, err = promql.ParseMetricSelector(metricSelector)
-	} else {
-		labelMatchers = make(metric.LabelMatchers, 0)
-	}
-	if err != nil {
-		return "", err
-	}
-	return "{" + metric.LabelMatchers(append(labelMatchers, matcher)).String() + "}", nil
-}
-
-// labelInjector enhances every reference to a metric (vector-selector) with an additional label-constraint
-// We use this to restrict a query to metrics belonging to a single OpenStack tenants stored in label 'project_id'
 type labelInjector struct {
 	promql.Visitor
 	matcher *metric.LabelMatcher
 }
 
-// Visit does the actual modifications to PromQL expression nodes
 func (v labelInjector) Visit(node promql.Node) (w promql.Visitor) {
 	switch node.(type) {
-	case *promql.MatrixSelector:
-		sel := node.(*promql.MatrixSelector)
-		sel.LabelMatchers = metric.LabelMatchers(append(sel.LabelMatchers, v.matcher))
 	case *promql.VectorSelector:
 		sel := node.(*promql.VectorSelector)
 		sel.LabelMatchers = metric.LabelMatchers(append(sel.LabelMatchers, v.matcher))
