1985ac59534467a5692b9e6a481c1ffe09b13dd3
Joachim Barheine <joachim.barheine@sap.com>
2017-06-09 22:15:26 +0200

    add test for promqlmod class

--- pkg/util/promqlmod_test.go
+++ pkg/util/promqlmod_test.go
@@ -5,15 +5,23 @@ import (
 	"testing"
 )
 
-const expected = "1 - sum(blackbox_api_status_gauge{check=~\"$api\",project_id=\"ecdc9fc4165d49b78987bbfbd5b4c9e2\"})"
+const expectedSelector = "{check=~\"$api\",project_id=\"ecdc9fc4165d49b78987bbfbd5b4c9e2\"}"
+const expectedExpr = "1 - sum(blackbox_api_status_gauge" + expectedSelector + ")"
 
-func TestLabelInjector_VisitInjector(t *testing.T) {
+func TestAddLabelConstraintToExpression(t *testing.T) {
 	testpql, err := promql.ParseExpr("1 - sum(blackbox_api_status_gauge{check=~\"$api\"})")
 	if err != nil {
 		t.Fatal(err)
 	}
-	AddLabelConstraint(testpql, "project_id", "ecdc9fc4165d49b78987bbfbd5b4c9e2")
-	if testpql.String() != expected {
-		t.Errorf("Unexpected result: %s; should have been %s", testpql.String(), expected)
+	AddLabelConstraintToExpression(testpql, "project_id", "ecdc9fc4165d49b78987bbfbd5b4c9e2")
+	if testpql.String() != expectedExpr {
+		t.Errorf("Unexpected result: %s; should have been %s", testpql.String(), expectedExpr)
+	}
+}
+
+func TestAddLabelConstraintToSelector(t *testing.T) {
+	result := AddLabelConstraintToSelector("{check=~\"$api\"}", "project_id", "ecdc9fc4165d49b78987bbfbd5b4c9e2")
+	if result != expectedSelector {
+		t.Errorf("Unexpected result: %s; should have been %s", result, expectedSelector)
 	}
 }
