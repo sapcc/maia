0844458e2c0882f25294bc425c345a178adc8383
Joachim Barheine <joachim.barheine@sap.com>
2017-06-22 14:55:04 +0200

    add missing certs; fill policy file; change maintainer to me

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -71,7 +71,7 @@ func init() {
 	// Cobra supports Persistent Flags, which, if defined here,
 	// will be global for your application.
 
-	RootCmd.PersistentFlags().StringVar(&cfgFile, "os-keystone-url", "", "OpenStack Authentication URL")
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "os-auth-url", "", "OpenStack Authentication URL")
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "os-username", "", "OpenStack Username")
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "os-password", "", "OpenStack Password")
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "os-user-domain-name", "", "OpenStack User's domain name")
